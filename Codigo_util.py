87638143d2084a8a76dece3f6881d06881ac9bb5
enrijuliav <enrijuliav@gmail.com>
2025-02-18 18:06:00 +0100

    Update Codigo_util.py

--- Codigo_util.py
+++ Codigo_util.py
@@ -1,1 +1,25 @@
 #En este docuento escribimos funciones útiles a la hora de la elaboración del trabajo, extraídas de los notebooks
+
+#pandas es una librería que permite "high-performance, easy-to-use data structures and data analysis tools"
+#Usaremos pandas para leer los csv y poder representarlos
+import pandas as pd
+
+df = pd.read_csv('NOMBRE.csv',sep=';')
+df.head(LINE_Nº)
+df.shape
+df.describe()
+df.drop('NOMBRE_COL', axis=1)
+df.types
+
+#.mean(axis=0 [will give you the calculated value per column]) - returns the statistical mean
+#.median(axis=0 [will give you the calculated value per column]) - returns the statistical median
+#.mode(axis=0 [will give you the calculated value per column]) - returns the statistical mode
+#.count() - gives number of total values in column
+#.unique() - returns array of all unique values in that column
+#.value_counts() - returns object containing counts of unique values
+
+df.NOMBRE_COL.FUNCION()
+
+#Histogramas
+import matplotlib.pyplot as plt
+hist = plt.hist(df.NOMBRE_COL, bins = Nº_ RECTANGULOS, density = True)
