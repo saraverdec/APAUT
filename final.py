495ebe705b10edd044334b882f3bd960d2ea21c9
enrijuliav <enrijuliav@gmail.com>
2025-03-11 09:41:48 +0100

    Update final.py

--- final.py
+++ final.py
@@ -1,2 +1,7 @@
-#codigo :)
-#Aún mas código, que chulo
+#TASK: Using the 'Wisconsin Diagnostic Breast Cancer' employed in the previous PCA notebook (available in the ./data folder of this notebook),
+#the students will perform the following tasks:
+#a) Read the dataset;
+#b) Perform a PCA analysis over the data;
+#c) Project the data into the PC space;
+#d) Apply both k-means and GMM over the projected data;
+#e) Compare the obtained clusters with the real labels.
